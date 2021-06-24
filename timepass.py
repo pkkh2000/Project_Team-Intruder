3414cb6a6aec9851d3685ce88187dfa758d13307
Prateek Khandelwal <43922247+pkkh2000@users.noreply.github.com>
2021-06-24 18:04:38 +0530

    Updating DockerFile and Python file

---
+++ timepass.py
@@ -0,0 +1,10 @@
+
+import time
+
+while 2 > 1:
+    print("Welcome to Hogwards")
+    time.sleep(1)
+    print("harrypotter will save your life")
+    time.sleep(2)
+    print("*********************")
+    print("*********************")
